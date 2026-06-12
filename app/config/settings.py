7486c37f3fd199a029887de35b281faa70bea8b5
RuanLMBS <ulle-2003@hotmail.com>
2026-06-12 13:42:07 -0300

    refactor: refactoring the database config

--- app/config/settings.py
+++ app/config/settings.py
@@ -9,6 +9,6 @@ DB_NAME = os.getenv("DB_NAME", "bigdata")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql:/{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/postgres"
 
 SECRET_KEY = os.getenv("SECRET_KEY", "")
