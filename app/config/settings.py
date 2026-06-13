c4a756bb310ee9583411e9484c2f593933b280e9
RuanLMBS <ulle-2003@hotmail.com>
2026-06-12 23:11:49 -0300

    hotfix: removing debug and adding missing / on env

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,7 +14,7 @@ if not DATABASE_URL:
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
     
-    DATABASE_URL = f"postgresql:/{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 
--- app/database/database.py
+++ app/database/database.py
@@ -3,11 +3,6 @@ from app.config.settings import DATABASE_URL
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-print(f"===== INICIO DO DEBUG =====")
-print(f"TIPO DA VARIAVEL: {type(DATABASE_URL)}")
-print(f"VALOR EXATO: {repr(DATABASE_URL)}")
-print(f"===== FIM DO DEBUG =====")
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
