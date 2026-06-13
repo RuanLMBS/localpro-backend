dd2d5d41c2c851c4255134de926f36727fef9803
RuanLMBS <ulle-2003@hotmail.com>
2026-06-12 22:45:24 -0300

    t :q :q

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,13 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "bigdata")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 
-DATABASE_URL = f"postgresql:/{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/postgres"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_NAME = os.getenv("DB_NAME", "bigdata")
+    DB_USER = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+    
+    DATABASE_URL = f"postgresql:/{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 
